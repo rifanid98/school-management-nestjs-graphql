a372275a7b2f2dd070a594e34df1701935197629
rifanid98 <adninsijawa.office@gmail.com>
2021-06-15 06:21:42 +0700

    creating the lesson module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { gqlModuleOptions } from './configs/graphql.config';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [GraphQLModule.forRoot(gqlModuleOptions)],
+  imports: [GraphQLModule.forRoot(gqlModuleOptions), LessonModule],
   controllers: [AppController],
   providers: [AppService],
 })
