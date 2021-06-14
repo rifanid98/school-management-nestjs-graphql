ad815cc65e9b0af197204d974f663471ff83b841
rifanid98 <adninsijawa.office@gmail.com>
2021-06-15 06:54:32 +0700

    lesson service and createLesson method

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleOptions } from 'src/configs/typeorm.config';
+import { Lesson } from './lesson.entity';
 import { LessonResolver } from './lesson.resolver';
+import { LessonService } from './lesson.service';
 
 @Module({
-  providers: [LessonResolver],
+  imports: [TypeOrmModule.forFeature([Lesson])],
+  providers: [LessonResolver, LessonService],
 })
 export class LessonModule {}
